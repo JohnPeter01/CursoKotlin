dc5f0f98640c6d0410e768484adf51b346ab1c9d
João Pedro <joao.pedro.fehc@gmail.com>
2023-09-27 18:28:05 -0300

    Implementing Get Costumer endpoint

--- src/main/kotlin/com/mtgcards/controller/CustomerController.kt
+++ src/main/kotlin/com/mtgcards/controller/CustomerController.kt
@@ -1,13 +1,17 @@
 package com.mtgcards.controller
 
+import com.mtgcards.model.Customer
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customers")
 class CustomerController {
 
     @GetMapping
-    fun getCustomer(): String {
-        return "Ainda não temos clientes senhor(a)"
+    fun getAllCustomers(): Customer {
+        return Customer("1","João","joao.pedro.fehc@gmail.com");
     }
+
 }
