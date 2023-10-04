b711425f23302024dbc65e76f2ee6280ca388309
João Pedro <joao.pedro.fehc@gmail.com>
2023-10-04 13:24:20 -0300

    Adding FlayWay and first migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation ("org.flywaydb:flyway-core:8.5.13")
+	implementation ("org.flywaydb:flyway-mysql:8.5.13")
 	runtimeOnly("mysql:mysql-connector-java:8.0.25")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
